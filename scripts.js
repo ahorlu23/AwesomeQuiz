8205c0e9b252fbe71e0e7a64feed75b2af6493c0
ahorlu23 <nobleahorlu4@gmail.com>
2024-08-24 23:26:39 +0000

    Continue button made active

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = info_box.querySelector(".quiz_box");
 
 //If start Quiz Button is clicked
 start_btn.onclick = ()=>{
@@ -12,4 +13,10 @@ start_btn.onclick = ()=>{
 //If Exit Button is clicked
 exit_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo");//hide the info box
+}
+
+//If Continue Button is clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");//hide the info box
+    quiz_box.classList.remove("activeQuiz");//Show the quiz box
 }
