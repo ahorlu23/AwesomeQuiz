01ee20db6a739795eaaecded7a561686b7406a30
ahorlu23 <nobleahorlu4@gmail.com>
2024-08-25 20:09:45 +0000

    Some work done on the numbering in js

--- scripts.js
+++ scripts.js
@@ -20,9 +20,11 @@ continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo");//hide the info box
     quiz_box.classList.add("activeQuiz");//Show the quiz box
     showQuestions(0);
+    queCounter(1);
 }
 
 let que_count = 0;
+let que_numb = 1;
 
 const next_btn = quiz_box.querySelector(".next_btn");
 
@@ -30,7 +32,9 @@ const next_btn = quiz_box.querySelector(".next_btn");
 next_btn.onclick = ()=>{
     if(que_count < quetions.length -1){
         que_count++;
+        que_numb++;
         showQuestions(que_count);
+        queCounter(que_numb);
     }else{
         console.log("Questions Completed");
     }
@@ -47,4 +51,10 @@ function showQuestions(index) {
                         +'<div class="option">'+ quetions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
+}
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>' + index + '</p>of<p>'+ quetions.length +'</p>Quetions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
