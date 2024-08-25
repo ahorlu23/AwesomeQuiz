6678cd5fe011d9a81e8048120b1c4527d31015a0
ahorlu23 <nobleahorlu4@gmail.com>
2024-08-25 00:15:23 +0000

    First 5 questions in quetions script

---
+++ quetions.js
@@ -0,0 +1,62 @@
+//Creating an array and passing the numbers, quetions, and answers
+let quetions = [
+    {
+        numb: 1,
+        quetion:"How do you write" + "Hello World" + "in an alert box?",
+        answer: "alert" + "(Hello World);",
+        options: [
+            "msg" + "(Hello World);",
+            "alertBox" + "(Hello World);",
+            "alert" + "(Hello World);",
+            "msgBox" + "(Hello World);"
+        ]
+    },
+
+    {
+        numb: 2,
+        quetion:"Which event occurs when the user clicks on an HTML element?",
+        answer: "onclick",
+        options: [
+            "onchange",
+            "onmouseover",
+            "onmouseclick",
+            "onclick"
+        ]
+    },
+
+    {
+        numb: 3,
+        quetion:"How do you declare a JavaScript variable?",
+        answer: "var carName;",
+        options: [
+            "variable carName;",
+            "var carName;",
+            "v carName;",
+            "string carName;"
+        ]
+    },
+
+    {
+        numb: 4,
+        quetion:"Which operator is used to assign a value to a variable?",
+        answer: "=",
+        options: [
+            "*",
+            "x",
+            "=",
+            "-"
+        ]
+    },
+
+    {
+        numb: 5,
+        quetion:"How do you create a function in JavaScript?",
+        answer: "function myFunction()",
+        options: [
+            "call myFunction()",
+            "myFunction()",
+            "call function myFunction()",
+            "Call.myFunction()"
+        ]
+    },
+];
