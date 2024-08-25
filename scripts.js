96484482caa6a30a223b10ce309736ed6e48e04b
ahorlu23 <nobleahorlu4@gmail.com>
2024-08-25 18:46:00 +0000

    Worked on the Next button

--- scripts.js
+++ scripts.js
@@ -19,14 +19,28 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo");//hide the info box
     quiz_box.classList.add("activeQuiz");//Show the quiz box
-    showQuestions();
+    showQuestions(0);
 }
 
 let que_count = 0;
 
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If the Next Que button is clicked
+next_btn.onclick = ()=>{
+    que_count++;
+    showQuestions(que_count);
+}
+
 //Getting questions and options from array 
-function showQuestions() {
+function showQuestions(index) {
     const que_text = document.querySelector(".que_text");
-    let que_tag = '<span>' + quetions[0].quetion + '</span>'
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>' + quetions[index].quetion + '</span>';
+    let option_tag = '<div class="option">'+ quetions[index].options[0] +'<span></span></div>'
+                        +'<div class="option">'+ quetions[index].options[1] +'<span></span></div>'
+                        +'<div class="option">'+ quetions[index].options[2] +'<span></span></div>'
+                        +'<div class="option">'+ quetions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
 }
